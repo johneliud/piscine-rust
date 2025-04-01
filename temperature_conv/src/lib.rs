72ce6f0fda090d5cfb74c386f7f7e1b107604cd2
johneliud <johneliud4@gmail.com>
2025-04-01 15:48:16 +0300

    fix: correct fahrenheit to celsius conversion formula

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    return (f - 32.0) * 5.0/9.0;
+    return (f - 32.0) / (9.0/5.0);
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    return (c * 9.0/5.0) + 32.0
+    return (c * (9.0/5.0)) + 32.0;
 }
 
 #[cfg(test)]
