6ba4020266d9e13b0a4e02bb097dc2586a4d8abd
johneliud <johneliud4@gmail.com>
2025-04-01 15:01:55 +0300

    refactor sum function to use u8 type and remove unused tests

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn sum(a: u32, b: u32) -> u32 {
+pub fn sum(a: u8, b: u8) -> u8 {
     return a + b;
 }
 
@@ -22,43 +22,6 @@ pub fn rem(a: f32, b: f32) -> f32 {
     if b == 0.0 {
         panic!("Cannot be 0.0")
     }
-    
+
     a % b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[should_panic]
-    fn sum_test() {
-        let sum_result = sum(2, 2);
-        assert_eq!(sum_result, 4);
-    }
-
-    #[test]
-    fn diff_test() {
-        let diff_result = diff(2,2);
-        assert_eq!(diff_result, 0);
-    }
-
-    #[test]
-    #[should_panic]
-    fn pro_test() {
-        let pro_result = pro(2,2);
-        assert_eq!(pro_result, 0);
-    }
-
-    #[test]
-    fn quo_test() {
-        let quo_result = quo(2.0, 2.0);
-        assert_eq!(quo_result, 1.0);
-    }
-
-    #[test]
-    fn rem_test() {
-        let rem_result = rem(2.0, 2.0);
-        assert_eq!(rem_result, 0.0);
-    }
 }
