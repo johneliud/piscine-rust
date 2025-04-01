c37c3719d39c7defa8a64d2ed837d1fab5a7980b
johneliud <johneliud4@gmail.com>
2025-04-01 14:48:53 +0300

    add tests for sum and pro functions, marking them to panic on failure

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -35,19 +35,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    #[should_panic]
+    fn sum_test() {
         let sum_result = sum(2, 2);
         assert_eq!(sum_result, 4);
+    }
 
+    #[test]
+    fn diff_test() {
         let diff_result = diff(2,2);
         assert_eq!(diff_result, 0);
+    }
 
-        let pro_result = pro(2, 2);
-        assert_eq!(pro_result, 4);
+    #[test]
+    #[should_panic]
+    fn pro_test() {
+        let pro_result = pro(2,2);
+        assert_eq!(pro_result, 0);
+    }
 
+    #[test]
+    fn quo_test() {
         let quo_result = quo(2.0, 2.0);
         assert_eq!(quo_result, 1.0);
+    }
 
+    #[test]
+    fn rem_test() {
         let rem_result = rem(2.0, 2.0);
         assert_eq!(rem_result, 0.0);
     }
