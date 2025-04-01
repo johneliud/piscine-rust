f5ea192cbdf568dce64f4be8d3d2a6099bbf3792
johneliud <johneliud4@gmail.com>
2025-04-01 13:43:12 +0300

    add sum function with unit test

---
+++ scalar/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = sum(2, 2);
+        assert_eq!(result, 4);
+    }
+}
