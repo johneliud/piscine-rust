87b0b9798cb8bbef7826286bf7ce2c5b8c1d0aa4
johneliud <johneliud4@gmail.com>
2025-04-02 07:00:55 +0300

    refactor riddle logic to improve user interaction and track attempts

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -2,18 +2,27 @@ use std::io;
 
 fn main() {
     let mut answer = String::new();
-
-    riddle(answer);
-}
-
-fn riddle(answer: String::new()) {
-    println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+    let mut tries = 0;
 
     loop {
-        io::stdin().read_line(&mut String).expect("Failed to read answer");
+        println!(
+            "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+        );
 
-        if answer == "e" {
+        answer.clear();
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read answer");
+        let trimmed_answer = answer.trim();
+
+        tries += 1;
+
+        if trimmed_answer == "e" {
+            println!("You are correct! It took you {} tries.", tries);
             break;
+        } else {
+            println!("You are incorrect!");
         }
     }
 }
+
