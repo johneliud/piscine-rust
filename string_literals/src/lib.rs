0a7695a6ff294671330f5cc84d92c5e770faa000
johneliud <johneliud4@gmail.com>
2025-04-03 13:44:46 +0300

    add is_ascii function to check if a string contains only ASCII characters

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -2,6 +2,10 @@ pub fn is_empty(v: &str) -> bool {
     v.chars().count() == 0
 }
 
+pub fn is_ascii(v: &str) -> bool {
+    v.chars().all(|c| c.is_ascii())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
