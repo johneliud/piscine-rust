4ea0d236231255bd31d2f50e05ed60d01a9e8667
johneliud <johneliud4@gmail.com>
2025-04-03 19:47:32 +0300

    add: str_function that returns a tuple with the original value, and the exponential function of each value as a string .

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -3,3 +3,8 @@ pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     let abs = exp.abs();
     (c, exp.exp(), abs.ln())
 }
+
+pub fn str_function(a: String) -> (String, String) {
+    let b = a.parse::<f64>().unwrap().exp().to_string();
+    (a, b)
+}
