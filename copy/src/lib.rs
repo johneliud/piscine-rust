e3714b041dcdc1c78dce6ae42681d024a10bdff0
johneliud <johneliud4@gmail.com>
2025-04-03 19:49:31 +0300

    add: Vec_function that returns a tuple with the original value, and the natural logarithm of each absolute value.

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -8,3 +8,13 @@ pub fn str_function(a: String) -> (String, String) {
     let b = a.parse::<f64>().unwrap().exp().to_string();
     (a, b)
 }
+
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let mut c = Vec::new();
+    let mut d = Vec::new();
+    for &x in b.iter() {
+        c.push(x);
+        d.push(x as f64);
+    }
+    (c, d)
+}
