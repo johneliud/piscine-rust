063e1fcdaa093318edbca1078d77a1a261452a93
johneliud <johneliud4@gmail.com>
2025-04-04 04:19:56 +0300

    feat: add diagonals function to check for winning condition in diagonals

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -5,3 +5,9 @@ pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
 pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
     (0..3).any(|col| (0..3).all(|row| table[row][col] == player))
 }
+
+pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
+    let diag1 = (0..3).all(|i| table[i][i] == player);
+    let diag2 = (0..3).all(|i| table[i][2 - i] == player);
+    diag1 || diag2
+}
