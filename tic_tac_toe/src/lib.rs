1f829f5c0e1134b857f87bc8d1e52229d4b1c337
johneliud <johneliud4@gmail.com>
2025-04-04 04:19:35 +0300

    feat: add vertical function to check for winning condition in columns

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
     table.iter().any(|row| row.iter().all(|&c| c == player))
 }
+
+pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
+    (0..3).any(|col| (0..3).all(|row| table[row][col] == player))
+}
