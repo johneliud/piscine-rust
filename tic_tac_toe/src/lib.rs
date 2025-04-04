891bc482d8bcc17356b4f29e79392aeae25b2db8
johneliud <johneliud4@gmail.com>
2025-04-04 04:20:10 +0300

    feat: add tic_tac_toe function to determine game outcome

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -11,3 +11,13 @@ pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
     let diag2 = (0..3).all(|i| table[i][2 - i] == player);
     diag1 || diag2
 }
+
+pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+    if horizontal('X', table) || vertical('X', table) || diagonals('X', table) {
+        "player X won".to_string()
+    } else if horizontal('O', table) || vertical('O', table) || diagonals('O', table) {
+        "player O won".to_string()
+    } else {
+        "tie".to_string()
+    }
+}
