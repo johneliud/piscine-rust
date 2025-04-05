6c25b98b1616597dc934665f2ff05a1e25bef751
johneliud <johneliud4@gmail.com>
2025-04-05 20:34:05 +0300

    add: implement median function to calculate the median of a list of integers

--- hashing/src/main.rs
+++ hashing/src/main.rs
@@ -1,3 +1,14 @@
 pub fn mean(list: &[i32]) -> f64 {
     list.iter().sum::<i32>() as f64 / list.len() as f64
 }
+
+pub fn median(list: &[i32]) -> i32 {
+    let mut l = list.to_vec();
+    l.sort();
+    let mid = l.len() / 2;
+    if l.len() % 2 == 0 {
+        (l[mid - 1] + l[mid]) / 2
+    } else {
+        l[mid]
+    }
+}
