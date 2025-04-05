394476188c5b75c4232b82a0ccc8501d1cec0e50
johneliud <johneliud4@gmail.com>
2025-04-05 11:15:10 +0300

    add: change_case function which converts the uppercase letters of a string into lowercase and the lowercase to uppercase.

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -17,3 +17,17 @@ pub fn title_case(input: &str) -> String {
         .collect::<Vec<_>>()
         .join(" ")
 }
+
+pub fn change_case(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().to_string()
+            } else {
+                c.to_lowercase().to_string()
+            }
+        })
+        .collect::<Vec<_>>()
+        .join("")
+}
