45c1edf2d7afa4f87e3db83301b05d2089389425
johneliud <johneliud4@gmail.com>
2025-04-10 18:23:52 +0300

    add: volume_fit function that checks if a given volume can fit a specified number of geometric shapes of a certain size.

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -19,3 +19,25 @@ pub fn area_fit(
 
     (required_area * times as f64) <= area as f64
 }
+
+pub fn volume_fit(
+    x: usize,
+    y: usize,
+    z: usize,
+    objects: GeometricalVolumes,
+    times: usize,
+    a: usize,
+    b: usize,
+    c: usize,
+) -> bool {
+    let volume = x * y * z;
+    let required_volume = match objects {
+        GeometricalVolumes::Cube => cube_volume(a) as f64,
+        GeometricalVolumes::Sphere => sphere_volume(a),
+        GeometricalVolumes::Cone => cone_volume(a, b),
+        GeometricalVolumes::Pyramid => triangular_pyramid_volume(a as f64, b),
+        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
+    };
+
+    (required_volume * times as f64) <= volume as f64
+}
