4bd0ec995b50577d03b8786feb5a5426c7868c00
johneliud <johneliud4@gmail.com>
2025-04-10 18:26:37 +0300

    add: circle_area function that calculates the area of a circle.

--- does_it_fit/src/areas_volumes.rs
+++ does_it_fit/src/areas_volumes.rs
@@ -19,3 +19,7 @@ pub fn square_area(side: usize) -> usize {
 pub fn triangle_area(base: usize, height: usize) -> f64 {
     (base as f64 * height as f64) / 2.0
 }
+
+pub fn circle_area(radius: usize) -> f64 {
+    std::f64::consts::PI * (radius.pow(2) as f64)
+}
