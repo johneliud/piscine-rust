d0b93c7dd8d03d294f05c8f75ff674085f4f2f4f
johneliud <johneliud4@gmail.com>
2025-04-18 18:50:32 +0300

    chore: Define the following structures:
    Workers: containing:
    drops: that will save the number of dropped threads.
    states: that will save the state of multiple threads. If the thread is not dropped, the state will be false, and will be true otherwise.
    Thread: containing:
    pid: the id of the thread.
    cmd: the name of the thread.
    parent: a link to the structure Workers. (Tip: this should be a reference).
    
    You'll need to also add the following associated functions to the structures:
    Workers :
    new: that creates a default worker.
    new_worker: that returns a tuple with the pid and a new Thread. This function must receive a String representing the cmd.
    is_dropped: that receives a pid and returns a bool that indicates the state of the thread.
    track_worker: which returns a usize representing the length of the states vector. (The index of the next new thread).
    add_drop: which is called by the Drop trait. It will receive a pid that will be used to change the state of the thread. If the state of that thread is true then it will panic with the message 'X is already dropped', where X represents the pid). Otherwise it should change the state to true and increment the drops field by 1.
    
    Thread:
    new_thread: that initializes a new thread.
    skill: that drops the thread.

---
+++ drop_the_thread/src/lib.rs
@@ -0,0 +1,71 @@
+use std::cell::{Cell, RefCell};
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub struct Workers {
+    pub drops: Cell<usize>,
+    pub states: RefCell<Vec<bool>>,
+}
+
+impl Workers {
+    pub fn new() -> Workers {
+        Workers {
+            drops: Cell::new(0),
+            states: RefCell::new(Vec::new()),
+        }
+    }
+
+    pub fn new_worker(&self, c: String) -> (usize, Thread) {
+        let pid = self.track_worker();
+        self.states.borrow_mut().push(false);
+        (pid, Thread::new_thread(pid, c, self))
+    }
+
+    pub fn track_worker(&self) -> usize {
+        self.states.borrow().len()
+    }
+
+    pub fn is_dropped(&self, id: usize) -> bool {
+        if id < self.states.borrow().len() {
+            self.states.borrow()[id]
+        } else {
+            false
+        }
+    }
+
+    pub fn add_drop(&self, id: usize) {
+        let mut states = self.states.borrow_mut();
+        if id < states.len() {
+            if states[id] {
+                panic!("{} is already dropped", id);
+            } else {
+                states[id] = true;
+                self.drops.set(self.drops.get() + 1);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Thread<'a> {
+    pub pid: usize,
+    pub cmd: String,
+    pub parent: &'a Workers,
+}
+
+impl<'a> Thread<'a> {
+    pub fn new_thread(p: usize, c: String, t: &'a Workers) -> Thread<'a> {
+        Thread {
+            pid: p,
+            cmd: c,
+            parent: t,
+        }
+    }
+
+    pub fn skill(self) {}
+}
+
+impl<'a> Drop for Thread<'a> {
+    fn drop(&mut self) {
+        self.parent.add_drop(self.pid);
+    }
+}
