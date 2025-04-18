e71744ddaf919e754ef6de43233cd6d585f134b3
johneliud <johneliud4@gmail.com>
2025-04-18 18:20:07 +0300

    add: new function that will initialize the WorkEnvironment with grade set to none.

---
+++ box_recursion/src/lib.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+pub type Link = Option<Box<Worker>>;
+
+#[derive(Debug)]
+pub struct Worker {
+    pub role: String,
+    pub name: String,
+    pub next: Link,
+}
+
+impl WorkEnvironment {
+    pub fn new() -> WorkEnvironment {
+        WorkEnvironment { grade: None }
+    }
+}
